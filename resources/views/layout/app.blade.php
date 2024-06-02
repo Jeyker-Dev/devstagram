9420945bb578962287ad730a6899415b79f87b57
Jeyker-Dev <134793631+J0K3R1178@users.noreply.github.com>
2024-06-02 15:48:07 -0400

    Instalando Tailwindcss

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Devstagram - @yield('titulo')</title>
+
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        <h1 class="text-4xl font-extrabold">@yield('titulo')</h1>
+
+        <nav>
+            <a href="/">Inicio</a>
+            <a href="/welcome">Welcome</a>
+        </nav>
+    </body>
+</html>

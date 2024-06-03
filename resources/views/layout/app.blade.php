d040e59a9d59fdaa6ca216b9ed72e30240c1622a
Jeyker-Dev <134793631+J0K3R1178@users.noreply.github.com>
2024-06-02 23:04:38 -0400

    Creada Navegacion y Footer en la pagina principal, tambien se elimino la vista de welcome

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,14 +6,36 @@
 
         <title>Devstagram - @yield('titulo')</title>
 
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
         @vite('resources/css/app.css')
     </head>
-    <body>
-        <h1 class="text-4xl font-extrabold">@yield('titulo')</h1>
+<!--    Here End Head -->
+    <body class="bg-gray-100">
+        <header class="p-5 border-p bg-white shadow">
+            <div class="container mx-auto flex justify-between">
+                <h1 class="text-3xl font-black">Devstagram</h1>
 
-        <nav>
-            <a href="/">Inicio</a>
-            <a href="/welcome">Welcome</a>
-        </nav>
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear Cuenta</a>
+                </nav>
+<!--    Here End Nav -->
+            </div>
+        </header>
+<!--    Here End Header -->
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">
+                @yield('titulo')
+            </h2>
+
+            @yield('contenido')
+        </main>
+<!--    Here End Main -->
+        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+            Devstagram - Todos los Derechos Reservados
+            {{ now()->year}}
+        </footer>
+<!--    Here End Footer -->
     </body>
 </html>
